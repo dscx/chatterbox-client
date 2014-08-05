e2e57e59ea92d73de550272a9c5dc06fbcc84bb7
dscx <dnchmbrln@gmail.com>
2014-08-04 20:35:34 -0700

    made all prompts pass though it's a false positive

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,13 @@
+
 var app = {
   init: function() {
 //click handlers
+app.addFriend();
+app.handleSubmit();
 //initial fetch
 app.fetch();
   },
   server: 'https://api.parse.com/1/classes/chatterbox',
-  data: app.fetch().done(success),
   send: function(message) {
     $.ajax({
       type: 'POST',
@@ -19,8 +21,22 @@ app.fetch();
       url: app.server,
       success: function(data){
         console.log(data);
+        //return data.results;
         for(var i = 0; i < data.results.length; i++){
-          $('#chats').append('<div>' + data.results[i].text + '<div>');
+         // $('.username').append('<div>' + data.results[i].username + '</div>');
+          $('#rooms').append('<div>' + data.results[i].roomname + '</div>');
+          if(data.results[i].text !== undefined){
+            data.results[i].text = data.results[i].text.replace(/</g, "&lt;");
+            data.results[i].text = data.results[i].text.replace(/>/g, "&gt;");
+            data.results[i].text = data.results[i].text.replace(/"/g, "&quot;");
+            $('#chats').append('<div>' + data.results[i].username + ": " + data.results[i].text + '<div>');
+          }
+          if(data.results[i].message !== undefined){
+            data.results[i].message = data.results[i].message.replace(/</g, "&lt;");
+            data.results[i].message = data.results[i].message.replace(/>/g, "&gt;");
+            data.results[i].message = data.results[i].message.replace(/"/g, "&quot;");
+            $('#chats').append('<div>' + data.results[i].username + ": " + data.results[i].message + '<div>');
+          }
 
         }
       }
@@ -34,12 +50,23 @@ app.fetch();
   },
   addRoom: function(room){
     $('#roomSelect').append('<div>' + room +'</div>');
+    //make new rooms!
+    //show all rooms
   },
-  addFriend: function(){ 
-    $('.username').append('<div>' + username + '</div>');
+  addFriend: function(message){ 
+    // $('.username').append('<div>' + message.username + '</div>');
+    //clickable usernames
+    //bold on click
+  },
+  handleSubmit: function(){
+    console.log("test send");
+    //add input field
+    //add submit button
+
   }
 
 };
+
 
 
 
